e93e4727d4aeadafd7223c206d9dafe0436eaf1b
sh <lifeisvector@gmail.com>
2025-06-04 13:26:53 +0900

    [FEATURE] p24

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,5 +1,19 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
+    @Test
+    public void createGame() {
+        Game game = new Game();
+        assertNotNull(game);
+    }
 
+    @Test
+    public void throwExceptionWhenInputIsNull() {
+        assertThrows(IllegalArgumentException.class, () ->  {
+            Game game = new Game();
+            game.guess(null);
+        });
+    }
 }
