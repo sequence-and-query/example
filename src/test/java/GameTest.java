6dddd890847c76fb1e33f03668731e3fd511a85c
sh <lifeisvector@gmail.com>
2025-06-04 13:31:37 +0900

    [FEATURE] p28

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -22,4 +22,11 @@ class GameTest {
             game.guess(null);
         });
     }
+
+    @Test
+    public void throwExceptionWhenInputLengthIsUnmatched(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            game.guess("11");
+        });
+    }
 }
